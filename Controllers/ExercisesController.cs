d291e981e6eed0035f857f508c2018ff67d02aa3
Natasha Cox <natashac1014@gmail.com>
2018-09-11 10:12:36 -0500

    first commit

---
+++ Controllers/ExercisesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StudentExercises.Models;
+
+namespace StudentExercises.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ExercisesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+
+            using (IDbConnection conn = Connection)
+            {
+                string sql = "SELECT * FROM Exercise";
+
+                var fullExercises = await conn.QueryAsync<Exercise>(sql);
+                return Ok(fullExercises);
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            using (IDbConnection conn = Connection)
+            {
+                string sql = $"SELECT * FROM Exercise WHERE Id = {id}";
+
+                var theSingleExercise = (await conn.QueryAsync<Exercise>(sql)).Single();
+                return Ok(theSingleExercise);
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Exercise exercise)
+        {
+            string sql = $@"INSERT INTO Exercise"
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
